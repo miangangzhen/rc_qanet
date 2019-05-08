1a19d28b90978b7110a192e01cc362fa41295afc
miangangzhen <miangangzhen@sina.com>
2019-05-08 08:51:23 +0800

    Mod: upload w2v vocab model

--- tensorflow/my_code/my_prepare.py
+++ tensorflow/my_code/my_prepare.py
@@ -1,12 +1,10 @@
 import logging
 import tensorflow as tf
-from dataset import BRCDataset
 import collections
 from vocab import Vocab
 import os
 import pickle
 import json
-from collections import Counter
 
 """
 用这个程序替代官方run.py里的prepare方法。
@@ -23,66 +21,14 @@ class Param(collections.namedtuple("param",
 
 
 # override /DuReader/tensorflow/dataset.BRCDataset._load_dataset + word_iter
-def load_dataset(path, param, train=False):
-    for data_path in tf.gfile.Glob(path):
-        with open(data_path, "r", encoding="utf-8") as fin:
-            for lidx, line in enumerate(fin):
-                sample = json.loads(line.strip())
-                if train:
-                    if len(sample['answer_spans']) == 0:
-                        continue
-                    if sample['answer_spans'][0][1] >= param.max_p_len:
-                        continue
-
-                if 'answer_docs' in sample:
-                    sample['answer_passages'] = sample['answer_docs']
-
-                sample['question_tokens'] = sample['segmented_question']
-
-                sample['passages'] = []
-                for d_idx, doc in enumerate(sample['documents']):
-                    if train:
-                        most_related_para = doc['most_related_para']
-                        sample['passages'].append(
-                            {'passage_tokens': doc['segmented_paragraphs'][most_related_para],
-                             'is_selected': doc['is_selected']}
-                        )
-                    else:
-                        para_infos = []
-                        for para_tokens, p_tokens, title, title_tokens in zip(doc['segmented_paragraphs'],
-                                                                              doc['paragraphs'], doc['title'],
-                                                                              doc['segmented_title']):
-                            question_tokens = sample['segmented_question']
-                            p_char_tokens = [i for i in p_tokens]
-                            title_char_tokens = [i for i in title]
-                            q_char_tokens = [i for i in sample['question']]
-
-                            # 这里改过了，和原版是不一样的，计算recall_wrt_question分数的方法被修改了
-                            common_with_question = Counter(para_tokens) & Counter(question_tokens)
-                            common_with_question_char = Counter(p_char_tokens) & Counter(q_char_tokens)
-                            title_common_with_question = Counter(title_tokens) & Counter(question_tokens)
-                            title_common_with_question_char = Counter(title_char_tokens) & Counter(q_char_tokens)
-
-                            correct_preds = sum(common_with_question.values())
-                            recall_eq = 0 if correct_preds == 0 else float(correct_preds) / len(question_tokens)
-
-                            correct_preds = sum(common_with_question_char.values())
-                            recall_eq_char = 0 if correct_preds == 0 else float(correct_preds) / len(q_char_tokens)
-
-                            correct_preds = sum(title_common_with_question.values())
-                            recall_tq = 0 if correct_preds == 0 else float(correct_preds) / len(question_tokens)
-
-                            correct_preds = sum(title_common_with_question_char.values())
-                            recall_tq_char = 0 if correct_preds == 0 else float(correct_preds) / len(q_char_tokens)
-
-                            recall_wrt_question = float(recall_eq + recall_eq_char) / 2
-                            para_infos.append((para_tokens, recall_wrt_question, len(para_tokens)))
-                        para_infos.sort(key=lambda x: (-x[1], x[2]))
-                        fake_passage_tokens = []
-                        for para_info in para_infos[:1]:
-                            fake_passage_tokens += para_info[0]
-                        sample['passages'].append({'passage_tokens': fake_passage_tokens})
-                yield sample
+def load_dataset(paths, param, train=False):
+    for path in paths:
+        for data_path in tf.gfile.Glob(path):
+            print(data_path)
+            with open(data_path, "r", encoding="utf-8") as fin:
+                for lidx, line in enumerate(fin):
+                    sample = json.loads(line.strip())
+                    yield sample
 
 
 # override /DuReader/tensorflow/run.prepare
@@ -96,19 +42,20 @@ def prepare(args):
         assert tf.gfile.Exists(data_path), '{} file does not exist.'.format(data_path)
 
     logger.info('Preparing the directories...')
-    for dir_path in [args.vocab_dir, args.model_dir, args.result_dir, args.summary_dir]:
+    for dir_path in [args.vocab_dir]:
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
     logger.info('Building vocabulary...')
-    brc_data = load_dataset(param.train_files, param, train=True)
+    brc_data = load_dataset([args.train_files, args.dev_files, args.test_files], args, train=True)
     vocab = Vocab(lower=True)
     for sample in brc_data:
-        for token in sample['question_tokens']:
+        for token in sample['segmented_question']:
             vocab.add(token)
-        for passage in sample['passages']:
-            for token in passage['passage_tokens']:
-                vocab.add(token)
+        for doc in sample['documents']:
+            for p in doc['segmented_paragraphs']:
+                for token in p:
+                    vocab.add(token)
 
     unfiltered_vocab_size = vocab.size()
     vocab.filter_tokens_by_cnt(min_cnt=10)
@@ -118,7 +65,7 @@ def prepare(args):
 
     logger.info('Assigning embeddings...')
     if args.use_embd:
-        vocab.load_pretrained_embeddings("100_ver_not_pure.txt")
+        vocab.load_pretrained_embeddings("w2v_pe.txt")
     else:
         vocab.randomly_init_embeddings(args.embed_size)
 
@@ -134,18 +81,22 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger("brc")
 
-    working_dir = "../../data/"
-    param = Param(["../../../../data/train_preprocessed/trainset/*"],
-                  dev_files=[],
-                  test_files=[],
-                  vocab_dir=working_dir+"vocab/",
-                  model_dir=working_dir+"models/",
-                  result_dir=working_dir+"results/",
-                  summary_dir=working_dir+"summary/",
-                  max_p_num=5,
-                  max_p_len=500,
-                  max_q_len=60,
-                  embed_size=100,
-                  use_embd=True)
+    working_dir = "./"
+    # param = Param(["../data/train_preprocessed/trainset/*.json_pe"],
+    #               dev_files=["../data/dev_preprocessed/devset/*.json_pe"],
+    #               test_files=["../data/test1_preprocessed/test1set/*.json_pe"],
+    #               vocab_dir=working_dir+"vocab/",
+    #               model_dir=working_dir+"models/",
+    #               result_dir=working_dir+"results/",
+    #               summary_dir=working_dir+"summary/",
+    #               max_p_num=5,
+    #               max_p_len=800,
+    #               max_q_len=60,
+    #               embed_size=100,
+    #               use_embd=True)
+    #
+    # prepare(param)
 
-    prepare(param)
+    with open("resource/vocab/vocab.data", "rb") as f:
+        vocab = pickle.load(f)
+    pass
--- tensorflow/my_code/pretrain_w2v.py
+++ tensorflow/my_code/pretrain_w2v.py
@@ -1,5 +1,6 @@
 import gensim
 import json
+import tqdm
 """
 预训练词向量
 """
@@ -10,38 +11,40 @@ class MySentences(object):
 
     def __iter__(self):
         for fname in self.dirnames:
-            for line in open(fname, "r", encoding="utf-8"):
-                obj = json.loads(line.strip())
-                ques_sent = obj['segmented_question']
-                yield ques_sent
-                for doc in obj['documents']:
-                    title_sent = doc['segmented_title']
-                    yield title_sent
-                    for p in doc['segmented_paragraphs']:
-                        yield p
+            print(fname)
+            with open(fname, "r", encoding="utf-8") as f:
+                for line in tqdm.tqdm(f.readlines()):
+                    obj = json.loads(line.strip())
+                    ques_sent = obj['segmented_question']
+                    yield ques_sent
+                    for doc in obj['documents']:
+                        for p in doc['segmented_paragraphs']:
+                            yield p
 
 
 if __name__ == "__main__":
-    root = '../../../../data/'
-    s_train = root + 'train_preprocessed/trainset/search.train.json'
-    z_train = root + 'train_preprocessed/trainset/zhidao.train.json'
-    # s_dev = root +'devset/search.dev.json'
-    # z_dev = root+'devset/zhidao.dev.json'
-    # s_test = root +'testset/search.test.json'
-    # z_test = root +'testset/zhidao.test.json'
+    root = '../data/'
+    s_train = root + 'train_preprocessed/trainset/search.train.json_pe'
+    z_train = root + 'train_preprocessed/trainset/zhidao.train.json_pe'
+    s_dev = root + 'dev_preprocessed/devset/search.dev.json_pe'
+    z_dev = root + 'dev_preprocessed/devset/zhidao.dev.json_pe'
+    s_test = root +'test1_preprocessed/test1set/search.test1.json_pe'
+    z_test = root +'test1_preprocessed/test1set/zhidao.test1.json_pe'
 
-    fs = [s_train, z_train] #, s_dev, z_dev, s_test, z_test]
+    fs = [s_train, z_train, s_dev, z_dev, s_test, z_test]
+    # fs = [s_dev, z_test]
+
     sents = MySentences(fs)
 
-    model = gensim.models.Word2Vec(iter=10, min_count=10, size=100, workers=6, negative=8, window=5)
+    # mod 20190507
+    # sg=1 skip-gram
+    model = gensim.models.Word2Vec(iter=10, min_count=10, size=128, workers=6, negative=8, window=5, sg=1)
     model.build_vocab(sents)
 
     sents = MySentences(fs)
     model.train(sents, total_examples=model.corpus_count, epochs=model.epochs)
 
-    # gzip.open(filename, mode='rb', compresslevel=9, encoding=None, errors=None, newline=None)
-    # https://docs.python.org/3/library/gzip.html
-    with open('./100_ver_not_pure.txt', 'w', encoding='utf-8') as f:
+    with open('./w2v_pe.txt', 'w', encoding='utf-8') as f:
         for k in model.wv.vocab.keys():
             s = k + ' ' + ' '.join([str(i) for i in model.wv[k]])
             f.write(s + '\n')
